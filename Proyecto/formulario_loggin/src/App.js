6a252b0eb25e3e3100e8eaa0ad4080821b5e6a17
Frank Palomino <frank.palomino@gmail.com>
2021-04-21 21:47:15 +0200

    Con pagina de registro

--- Proyecto/formulario_loggin/src/App.js
+++ Proyecto/formulario_loggin/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import SignUpForm from "./components/SignUpForm";
+import RealSignUp from "./components/RealSignUp";
+
 
 function App() {
   return (
     <div className="container mt-5">
-      <SignUpForm />
+      {/* <SignUpForm /> */}
+      <RealSignUp />
     </div>
   );
 }
---
+++ Proyecto/formulario_loggin/src/components/RealSignUp.js
@@ -0,0 +1,69 @@
+import React, { Fragment } from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import MyTextInput from "../constants/MyTextInput";
+import MyPassword from "../constants/MyPassword";
+
+const RealSignUp = () => {
+  const botonRegistrate = () => {
+    window.location = "https://github.com/FrankPalomino/proyectoFinal";
+  };
+
+  return (
+    <Fragment>
+      <h1>Registation!</h1>
+      <Formik
+        initialValues={{
+          user: "",
+          password: "",
+        }}
+        validationSchema={Yup.object({
+          user: Yup.string().required("Introduce tu usuario."),
+          correo: Yup.string().required("Introduce tu correo."),
+          password: Yup.string().required("Introduce la contraseña."),
+        })}
+        onSubmit={(values) => {
+          console.log(values);
+          window.location =
+            "https://www.divinacocina.es/Categorias/recetas-de-la-abuela/";
+        }}
+      >
+        <Form>
+          <MyTextInput
+            label="Usuario"
+            name="user"
+            type="text"
+            placeholder="Usuario"
+          />
+
+          <MyTextInput
+            label="Correo"
+            name="correo"
+            type="text"
+            placeholder="Correo"
+          />
+
+          <MyPassword
+            label="Contraseña"
+            name="password"
+            type="password"
+            placeholder="password"
+          />
+
+          <MyPassword
+            label="Repite Contraseña"
+            name="password2"
+            type="password"
+            placeholder="repite password"
+          />
+
+          <button type="submit" className="btn btn-primary">
+            Registrarse
+          </button>
+        </Form>
+      </Formik>
+    </Fragment>
+  );
+};
+
+export default RealSignUp;
